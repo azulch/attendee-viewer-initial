7a485dcd8d6a73bfef5b9561339d4e8028f01784
azulch <alexander.zulch@cgius.com>
2017-09-14 13:53:00 -0600

    Oauth2

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
@@ -5,17 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 
-public class AttendeeViewerApplication {
+public class AttendeeViewerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendeeViewerApplication.class, args);
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	        .antMatchers("/login").permitAll()
+	        .anyRequest().authenticated();
+	}
+
 	
 
 }
