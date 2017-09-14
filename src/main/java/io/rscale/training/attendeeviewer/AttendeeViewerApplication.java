f61a7de304e6e1e3e5fbde5482d245cd75af0d0b
azulch <alexander.zulch@cgius.com>
2017-09-14 14:10:38 -0600

    Enable oauth2a

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
@@ -2,6 +2,7 @@ package io.rscale.training.attendeeviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -12,7 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-
+@EnableOAuth2Sso
 public class AttendeeViewerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
