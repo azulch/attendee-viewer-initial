5cf1ba58c6abe83f304ef0794ee3fb1364a8b968
azulch <alexander.zulch@cgius.com>
2017-09-14 09:40:39 -0600

    initial
    changes to company and roster clients

--- src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
+++ src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
@@ -1,7 +1,11 @@
 package io.rscale.training.attendeeviewer;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+@FeignClient(name = "company")
 public interface CompanyClient {
 
 	Company getCompany(String id);
+	
 	
 }
